2730c94acfbf391d8ecaf3d7113fb294957e4816
Dave Winniford <dwinniford@gmail.com>
2020-01-27 16:19:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,11 @@ class School
     @roster = {}
   end 
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name 
+    if @roster.include?(grade)
+      @roster[grade] << name 
+    else 
+      @roster[grade] = []
+      @roster[grade] << name 
+    end 
   end 
 end 
